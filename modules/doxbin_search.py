cd361218b58eba67b6e0bb9352f4d74fbea74c37
Alex <146616478+Al3xUI@users.noreply.github.com>
2024-08-05 19:02:32 +0200

    Update doxbin_search.py

--- modules/doxbin_search.py
+++ modules/doxbin_search.py
@@ -2,11 +2,24 @@ import requests
 
 args = input("Enter search term >>> ")
 
-class doxbin_search():
-    def __init__(self):
-        r = requests.get("https://doxbin.com/search/q?="+args)
+class DoxbinSearch:
+    def __init__(self, term):
+        self.term = term
+        self.search()
 
-        print("resulat : "+r.json())
+    def search(self):
+        try:
+            r = requests.get(f"https://doxbin.com/upload/{self.term}")
+            if r.status_code == 200:
+                print("Result found:\n")
+                print(r.text)
+            elif r.status_code == 404:
+                print("Error: No result found (404).")
+            else:
+                print(f"Error: Received unexpected status code {r.status_code}.")
+        except requests.RequestException as e:
+            print(f"Error: An exception occurred: {e}")
+            return args
 
 if __name__ == "__main__":
-    doxbin_search()
+    DoxbinSearch(args)
