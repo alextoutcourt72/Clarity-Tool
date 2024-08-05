b371b008c3d4ee384911bdb8cfd65fa78478cce1
Alex <146616478+Al3xUI@users.noreply.github.com>
2024-08-05 18:26:22 +0200

    Update doxbin_search.py

--- modules/doxbin_search.py
+++ modules/doxbin_search.py
@@ -6,7 +6,7 @@ class doxbin_search():
     def __init__(self):
         r = requests.get("https://doxbin.com/search/q?="+args)
 
-        print("resulat : "+r.url)
+        print("resulat : "+r.json())
 
 if __name__ == "__main__":
     doxbin_search()
