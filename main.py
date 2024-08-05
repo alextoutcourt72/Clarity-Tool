bbbc911cf0715995b6b7c2fbd30f64981dd318c0
lllaaakkkiii <boutinclement210@gmail.com>
2024-08-05 18:24:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ Select tool :
 
         [1] Tool info       [6] PC info
         [2] Ip lookup       [7] Discord Token Info
-        [3] Whois lookup    
+        [3] Whois lookup    [8] Username Tracker
         [4] OSINT           
         [5] Web page saver (not work)
 """
